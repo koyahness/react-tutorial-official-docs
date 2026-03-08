3ecb5497bfeb2ac517193dd972d51ea54b9df357
koyahness <cryptomakoya@gmail.com>
2026-03-08 17:19:51 +0200

    update js files

---
+++ 3.Addingstyles.js
@@ -0,0 +1,13 @@
+//In React, you specify a CSS class with className. It works the same way as the HTML class attribute:
+<img className="avatar" />
+
+
+/*
+
+React does not prescribe how you add CSS files.
+
+In the simplest case, you’ll add a <link> tag to your HTML.
+
+If you use a build tool or a framework, consult its documentation to learn how to add a CSS file to your project.
+
+*/
