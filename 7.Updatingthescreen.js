5da18bef6c946a4c5804d1481d70f5349e917778
koyahness <cryptomakoya@gmail.com>
2026-03-08 18:19:28 +0200

    update markup file js

---
+++ 7.Updatingthescreen.js
@@ -0,0 +1,31 @@
+/*
+Often, you’ll want your component to “remember” some information and display it. For example, maybe you want to count the number of times a button is clicked. To do this, add state to your component.
+
+First, import useState from React:
+*/
+
+import { useState } from 'react';
+
+//Now you can declare a state variable inside your component:
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+  function MyButton() {
+  const [count, setCount] = useState(0);
+
+
+  /*
+You’ll get two things from useState: the current state (count), and the function that lets you update it (setCount). You can give them any names, but the convention is to write [something, setSomething].
+
+The first time the button is displayed, count will be 0 because you passed 0 to useState(). When you want to change state, call setCount() and pass the new value to it. Clicking this button will increment the counter:
+  */
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
