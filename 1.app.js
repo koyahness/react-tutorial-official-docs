0faa2616fda65279eaba612d01081cd277f1ffbb
koyahness <cryptomakoya@gmail.com>
2026-03-14 11:43:43 +0200

    update app.js files

--- 1.app.js
+++ 1.app.js
@@ -6,9 +6,11 @@ function MyButton() {
   );
 }
 //my button is a component
+/*Notice that <MyButton /> starts with a capital letter. That’s how you know it’s a React component. React component names must always start with a capital letter, while HTML tags must be lowercase. */
 
 
 //Now that you’ve declared MyButton, you can nest it into another component:
+//The export default keywords specify the main component in the file
 export default function MyApp() {
   return (
     <div>
